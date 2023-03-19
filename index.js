4cd64d9fb9aba03775b66aef4df2d8c0ab145476
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-03-19 11:31:14 -0400

    started boilerplate for js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+/* HELPER FUNCTIONS */
+const handleSubmitQuery = () => {
+	sidebar = document.getElementByClassName("sidebar__list")
+	sidebar.
+}
+
+
+/* Registered Listeners */
+
+const submitButton = document.getElementsByClassName()
+
+submitButton.addEventListener("click", handleSubmitQuery);
